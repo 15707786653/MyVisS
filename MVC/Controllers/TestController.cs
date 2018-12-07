86c54fd4f85c9e296c19edffca6672e76d842b73
15707786653 <945128701@qq.com>
2018-12-07 09:41:36 +0800

    2

---
+++ MVC/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public string GetString()
+        {
+            return "hello world MVC";
+        }
+        public Customer getCustomer()
+        {
+            Customer ct = new Customer();
+            ct.CustomerName = "abc";
+            ct.Address = "def";
+            return ct;
+            
+        }
+        public ActionResult GetView()
+        {
+            return null;
+        }
+    }
+}
---
+++ MVC/Models/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class Customer
+    {
+        public string CustomerName
+        {
+            get;
+            set;
+        }
+        public string Address
+        {
+            get;
+            set;
+        }
+        public override string ToString()
+        {
+            return this.CustomerName + "_" + this.Address;
+        }
+    }
+}
