6e8fcb23b19ab8de28d3b0a6ab73fb7d7549f7b0
15707786653 <945128701@qq.com>
2018-12-08 08:13:51 +0800

    yy

--- MVC/Controllers/TestController.cs
+++ MVC/Controllers/TestController.cs
@@ -24,7 +24,28 @@ namespace MVC.Controllers
         }
         public ActionResult GetView()
         {
-            return null;
+            //获取当前时间
+            string greeting;
+            
+            DateTime dt = DateTime.Now;
+            //获取当前小时数
+            int hour = dt.Hour;
+            
+            //根据小时数判断需要返回哪个视图，<12返回myview 否则返回mygetview>
+            if (hour > 12)
+            {
+                greeting = "早上好";
+            }
+            else
+            {
+                greeting = "下午好";
+            }
+            ViewData["greeting"] = greeting;
+            Employee emp = new Employee();
+            emp.Name = "石昊";
+            emp.Salary = 12138;
+            ViewData["EmpKey"] = emp;
+            return View("MyGetView");
         }
     }
 }
---
+++ MVC/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+    }
+}
