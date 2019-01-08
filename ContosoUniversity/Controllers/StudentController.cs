7ead5ada46ca5428b73ca783684fe630768520c0
15707786653 <9405128701@qq.com>
2019-01-08 18:12:04 +0800

    yyy

--- ContosoUniversity/Controllers/StudentController.cs
+++ ContosoUniversity/Controllers/StudentController.cs
@@ -9,6 +9,7 @@ using System.Web.Mvc;
 using ContosoUniversity.DAL;
 using ContosoUniversity.Models;
 using PagedList;
+using System.Data.Entity.Infrastructure;
 
 namespace ContosoUniversity.Controllers
 {
@@ -96,7 +97,7 @@ namespace ContosoUniversity.Controllers
                 return RedirectToAction("Index");
             }
             }
-            catch (DataException /* dex */)
+            catch (RetryLimitExceededException /* dex */)
             {
                 //Log the error (uncomment dex variable name and add a line here to write a log.
                 ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
--- ContosoUniversity/DAL/SchoolConfiguration.cs
+++ ContosoUniversity/DAL/SchoolConfiguration.cs
@@ -1,11 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure.Interception;
+using System.Data.Entity.SqlServer;
 
 namespace ContosoUniversity.DAL
 {
-    public class SchoolConfiguration
+    public class SchoolConfiguration : DbConfiguration
     {
+        public SchoolConfiguration()
+        {
+            SetExecutionStrategy("System.Data.SqlClient", () => new SqlAzureExecutionStrategy());
+            DbInterception.Add(new SchoolInterceptorTransientErrors());
+            DbInterception.Add(new SchoolInterceptorLogging());
+        }
     }
 }
